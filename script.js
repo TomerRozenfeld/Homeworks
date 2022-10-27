0833c3637b7a0f2f5bb58ed183db08e4928e6374
TomerRozenfeld <116294077+TomerRozenfeld@users.noreply.github.com>
2022-10-27 19:59:41 +0300

    Add files via upload
    שיעור 16+17

---
+++ script.js
@@ -0,0 +1,31 @@
+let x = 5;
+let y = 8;
+let z= x*y;
+ console.log(z);
+
+ let moviename="Doggo"; movierating=5.5; moviedirector="Tomer Rozenfeld";
+ console.log("BestDogMovie:",moviename," Score:",movierating," Producer:",moviedirector );
+
+ let length=50;
+ console.log(typeof(length));
+ let lastname="cohan";
+ console.log(typeof(lastname));
+ const person= {
+    firstname:"Avi",
+    lastname:"Cohen",
+ };
+ console.log(typeof(person));
+let b=10**5;
+console.log(b);
+let a=8+26*3*0+5;
+console.log(a);
+ let v=15-20;
+ console.log(v);
+ let h=100/5;
+ console.log(h);
+
+ x=15;
+ y=25;
+ console.log(x==y);
+
+ console.log(15==15);
